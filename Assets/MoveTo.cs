8aceaac24d95d66767eefc016dc7201230e699b8
SleepingAran <cheahpehao@gmail.com>
2017-10-27 12:11:15 +0800

    Version 0.0.1
    Updated asset requirement
    New AI script.

---
+++ Assets/MoveTo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour {
+
+    public Transform goal;
+    public float speed;
+    // Use this for initialization
+    void Start () {
+
+        speed = 0.5f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, goal.position, step);
+
+    }
+}
