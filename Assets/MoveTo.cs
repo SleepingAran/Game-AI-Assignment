d7851cdce8022a6125d2b5989f2bdbb1d80c8fa0
SleepingAran <cheahpehao@gmail.com>
2017-11-02 22:44:11 +0800

    Updated crowd AI
    updated AI script a tiny little bit

--- Assets/MoveTo.cs
+++ Assets/MoveTo.cs
@@ -8,14 +8,20 @@ public class MoveTo : MonoBehaviour {
     public float speed;
     // Use this for initialization
     void Start () {
-
-        speed = 0.5f;
+        
+        speed = Random.Range(0.5f,2f);
     }
 	
 	// Update is called once per frame
 	void Update () {
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, goal.position, step);
-
+        
+        RaycastHit hit; //check when hit
+        Debug.DrawRay(transform.position, transform.forward, Color.green);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 0.2f) && hit.collider.tag == "wall")
+        {
+            transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y + 2, transform.rotation.z,transform.rotation.w);
+        }
     }
 }
