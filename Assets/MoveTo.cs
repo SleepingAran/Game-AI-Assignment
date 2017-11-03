595cd08ee3e3f167316cc568bad8254310cd0fbf
SleepingAran <cheahpehao@gmail.com>
2017-11-03 10:48:34 +0800

    Revert "Updated crowd AI"
    This reverts commit d7851cdce8022a6125d2b5989f2bdbb1d80c8fa0.

--- Assets/MoveTo.cs
+++ Assets/MoveTo.cs
@@ -8,20 +8,14 @@ public class MoveTo : MonoBehaviour {
     public float speed;
     // Use this for initialization
     void Start () {
-        
-        speed = Random.Range(0.5f,2f);
+
+        speed = 0.5f;
     }
 	
 	// Update is called once per frame
 	void Update () {
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, goal.position, step);
-        
-        RaycastHit hit; //check when hit
-        Debug.DrawRay(transform.position, transform.forward, Color.green);
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 0.2f) && hit.collider.tag == "wall")
-        {
-            transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y + 2, transform.rotation.z,transform.rotation.w);
-        }
+
     }
 }
